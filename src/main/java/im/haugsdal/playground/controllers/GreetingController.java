ad836ed5321f119a839bc3c08fb095b0e12551c1
Markus Johan Haugsdal <markus.haugsdal@evry.com>
2020-01-12 18:36:30 +0100

    application props jsp

--- src/main/java/im/haugsdal/playground/controllers/GreetingController.java
+++ src/main/java/im/haugsdal/playground/controllers/GreetingController.java
@@ -1,14 +1,22 @@
 package im.haugsdal.playground.controllers;
 
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Controller;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 
-@RestController
+import java.util.Map;
+
+@Controller
 public class GreetingController {
 
-    @RequestMapping("/")
-    public String index() {
-        return "Greetings from Spring Boot!";
-    }
+    // inject via application.properties
+    @Value("${welcome.message:test}")
+    private String message = "Hello World";
 
+    @RequestMapping("/")
+    public String welcome(Map<String, Object> model) {
+        model.put("message", this.message);
+        return "welcome";
+    }
 }
