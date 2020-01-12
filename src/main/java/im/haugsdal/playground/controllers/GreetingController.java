8ae7f40d9e0e0a44c82d8e081f0946dbcdc03b46
Markus Johan Haugsdal <markus.haugsdal@evry.com>
2020-01-12 16:22:32 +0100

    first commit

---
+++ src/main/java/im/haugsdal/playground/controllers/GreetingController.java
@@ -0,0 +1,14 @@
+package im.haugsdal.playground.controllers;
+
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RestController
+public class GreetingController {
+
+    @RequestMapping("/")
+    public String index() {
+        return "Greetings from Spring Boot!";
+    }
+
+}
